3d5f42de5549c96af2f3269b5b6020a4be73a5f1
Sulman Baig <sulmanweb@gmail.com>
2018-03-19 13:12:55 +0500

    Adding swagger docs and ui to project

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # Error handling
   match '/404', to: 'error/errors#not_found', via: :all
   match '/500', to: 'error/errors#internal_server_error', via: :all
+  get '/api-docs/v1' => redirect('/dist/index.html?url=/apidocs/apidocs_v1.json')
   namespace :v1, defaults: {format: 'json'} do
     namespace :auth do
       post 'sign_up', to: 'registrations#create'
