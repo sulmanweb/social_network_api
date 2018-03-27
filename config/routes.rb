37bf60c1ecb77a03362586358a1eee0d703878e8
Sulman Baig <sulmanweb@gmail.com>
2018-03-27 13:36:12 +0500

    Adding timeline service

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+
   # Error handling
   match '/404', to: 'error/errors#not_found', via: :all
   match '/500', to: 'error/errors#internal_server_error', via: :all
   get '/api-docs/v1' => redirect('/dist/index.html?url=/apidocs/apidocs_v1.json')
   namespace :v1, defaults: {format: 'json'} do
+    get 'home', to: 'home#timeline'
     namespace :auth do
       post 'sign_up', to: 'registrations#create'
       post 'sign_in', to: 'sessions#create'
